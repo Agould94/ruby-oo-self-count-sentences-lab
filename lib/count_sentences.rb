26aaf501316539b09511be96bf1516b81b7e843a
Alexander Gould <alexander.gould94@gmail.com>
2021-02-19 10:54:17 -0800

    commit tree a158ee6525d8038a415cfb435d6cc8bd189e7408

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    split_scentences = self.split(/[.!?]\s/)
+    return split_scentences.count
   end
 end
